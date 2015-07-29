c25c8c7827554cdb05a52a0e2ee0bf28cac8dd2f
Akhil Acharya <akhilcacharya@gmail.com>
2015-07-28 21:18:16 -0400

    Initial Commit

---
+++ ApporiumTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApporiumTest
+//
+//  Created by Akhil Acharya  on 7/28/15.
+//  Copyright (c) 2015 Akhil Acharya . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApporiumTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApporiumTest
+//
+//  Created by Akhil Acharya  on 7/28/15.
+//  Copyright (c) 2015 Akhil Acharya . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
